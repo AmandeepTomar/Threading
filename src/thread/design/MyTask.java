76c4bfa37da60cb0a5269bc27908901997a63ddd
Amandeep Tomar <>
2020-04-06 00:30:57 +0530

    Changes in thread and task class

--- src/thread/design/MyTask.java
+++ src/thread/design/MyTask.java
@@ -9,7 +9,7 @@ public class MyTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i <1000 ; i++) {
-            System.out.print("T");
+            System.out.print("MyTask");
         }
     }
 }
--- src/thread/design/MyThread.java
+++ src/thread/design/MyThread.java
@@ -7,7 +7,7 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i <1000 ; i++) {
-            System.out.print("M");
+            System.out.print("MyThread");
         }
     }
 }
